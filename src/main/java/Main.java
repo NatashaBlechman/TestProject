38219e53db7f6b57fdb3eb0c5334e34d8adea39d
NatashaBlechman <nata.carteblanche@gmail.com>
2018-10-07 16:37:06 +0200

    add small program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a five-digit number: ");
+        int enter = scanner.nextInt();
+        String text = Integer.toString(enter);
+        int length = text.length();
+        int i=0;
+        while (i<length){
+            char result = text.charAt(i++);
+            System.out.println(result);
+        }
+    }
 }
